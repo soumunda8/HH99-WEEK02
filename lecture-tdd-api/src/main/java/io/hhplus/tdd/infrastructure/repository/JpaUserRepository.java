199df73e5f14db7047eca4970bf5488f5658646c
soumunda8 <soumunda8@naver.com>
2024-10-03 20:42:27 +0900

    REMOVE : 사용자관련 삭제

--- lecture-tdd-api/src/main/java/io/hhplus/tdd/infrastructure/repository/JpaUserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.hhplus.tdd.infrastructure.repository;
-
-import io.hhplus.tdd.domain.user.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
-}
